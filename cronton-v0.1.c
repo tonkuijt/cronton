0ee4ff76bef1c59ea5de18d9dc0e57cc1ee28176
Ton Kuijt <info@tonkuijt.nl>
2019-12-02 16:36:52 +0100

    Renamed main C file to avoid overwriting...

---
+++ cronton-v0.1.c
@@ -0,0 +1,39 @@
+/* cronton
+* 
+* This little thing checks if the time matches the wanted time.
+* If so, execute given command. Ugly, but works (ish)
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+void delay(int number_of_seconds) 
+{ 
+    int milli_seconds = 1000 * number_of_seconds; 
+    clock_t start_time = clock(); 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
+int main ()
+{
+int a = 1;
+while ( a == a )
+  {
+	time_t now;
+	struct tm *now_tm;
+	now = time(NULL);
+	now_tm = localtime(&now);
+	int hour = now_tm->tm_hour;
+	int minute = now_tm->tm_min;
+	int trigger_hour=16;
+	int trigger_minute=43;
+
+	if (( hour == trigger_hour ) & ( minute == trigger_minute)){
+		system("/usr/bin/logger CRONTON HAS RAN");
+	} 
+  delay(60);
+  }
+}
