f74b8a92985bf46bb93b9912e79e9617113e4c8a
Ton Kuijt <info@tonkuijt.nl>
2019-12-02 16:49:09 +0100

    Work in progress, does not compile atm

--- cronton.c
+++ cronton.c
@@ -10,16 +10,60 @@
 #include <time.h>
 #include <stdlib.h>
 
+const char * config()
+{
+        if (access("cronton.conf", F_OK) != -1 ){
+                return "./cronton.conf";
+        } else if (access("/etc/cronton/cronton.conf", F_OK) != -1 ){
+                return "/etc/cronton/cronton.conf";
+        } else {
+                printf("Config file not found!\n");
+                exit(1);
+        }
+        /* Nothing found, notify main process */
+}
+
 void delay(int number_of_seconds) 
 { 
-    int milli_seconds = 1000 * number_of_seconds; 
-    clock_t start_time = clock(); 
-    while (clock() < start_time + milli_seconds) 
+	int milli_seconds = 1000 * number_of_seconds; 
+	clock_t start_time = clock(); 
+	while (clock() < start_time + milli_seconds) 
         ; 
 } 
+
 int main ()
 {
+/* First, init ALL the variables! */
+char filename[256];
+sprintf(filename,"%s", config());
+char line[256];
+int linenum=0;
+FILE *file;
+file=fopen(filename, "r");
+
 int a = 1;
+
+/* Finding and using the config line */
+
+while(fgets(line, 256, file) != NULL)
+{
+        char time[256], command[256];
+
+        linenum++;
+        if(line[0] == '#') continue;
+
+                if(sscanf(line, "%s %s", time,  command) != 2)
+                        {
+                        fprintf(stderr, "Syntax error, line %d\n", linenum);
+                        continue;
+                        }
+                char * hour = strtok(time, ":");
+                char * minute = strtok(NULL, ":");
+        }
+
+/* If all is found, run the rest please */
+/* NEEDED: SANITY CHECKING. You lazy bastard you. */
+
 while ( a == a )
   {
 	time_t now;
@@ -28,12 +72,14 @@ while ( a == a )
 	now_tm = localtime(&now);
 	int hour = now_tm->tm_hour;
 	int minute = now_tm->tm_min;
-	int trigger_hour=16;
-	int trigger_minute=43;
+	int trigger_hour=hour;
+	int trigger_minute=minute;
 
 	if (( hour == trigger_hour ) & ( minute == trigger_minute)){
 		system("/usr/bin/logger CRONTON HAS RAN");
 	} 
-  delay(60);
+	printf("Found something (%s) to do at %d:%d, waiting for it...\n", command, trigger_hour, trigger_minute);
+  delay(600);
   }
 }
+
